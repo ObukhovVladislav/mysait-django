2d3d869c75e2f740a9504ce3d8de439d4967425f
Vladislav Obv <VladislavObv@example.com>
2020-10-19 12:20:21 +0500

    mysait-django

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from myapp.models import ProductCategories, ComputersLaptopsAndSoftware
+from myapp.models import ProductCategories, ComputersLaptopsAndSoftware, HouseholdAppliance
 
 
 admin.site.register(ProductCategories)
 admin.site.register(ComputersLaptopsAndSoftware)
+admin.site.register(HouseholdAppliance)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -15,3 +15,11 @@ class ComputersLaptopsAndSoftware(models.Model):
     name = models.CharField(max_length=200)
     desc = models.TextField(blank=True)
     is_active = models.BooleanField(default=True)
+
+
+class HouseholdAppliance(models.Model):
+    category = models.ForeignKey(ProductCategories,
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
