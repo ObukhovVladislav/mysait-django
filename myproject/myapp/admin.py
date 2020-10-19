d7c0132292bb14d531c3c82fb7b3d41c0dd8f5de
Vladislav Obv <VladislavObv@example.com>
2020-10-19 11:54:45 +0500

    mysait-django

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from myapp.models import ProductCategories, ComputersLaptopsAndSoftware
+
+
+admin.site.register(ProductCategories)
+admin.site.register(ComputersLaptopsAndSoftware)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategories(models.Model):
+    name = models.CharField(max_length=128)
+    desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+# class ComputersLaptopsAndSoftware(models.Model):
+#     category = models.ForeignKey(ProductCategories,
+#                                  on_delete=models.CASCADE)
+#     name = models.CharField(max_length=200)
+#     desc = models.TextField(blank=True)
+#     is_active = models.BooleanField(default=True)
