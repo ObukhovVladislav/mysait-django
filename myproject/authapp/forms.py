17a878b17423b3ecf0f73a89e38bc47cab573a5d
Vladislav Obv <VladislavObv@example.com>
2020-11-09 15:00:01 +0500

    mysite-django

---
+++ myproject/authapp/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class LoginForm(AuthenticationForm):
+    pass
+    # class Meta:
+    #     model = User
+    #     fields = ('username', 'password')
